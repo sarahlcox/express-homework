bc389f007bba624ba1865095599b5603ca187352
sarahlcox <sarahl.cox.22@gmail.com>
2020-10-25 15:51:48 -0500

    Updated files

--- routes/notes.js
+++ routes/notes.js
@@ -1,7 +1,9 @@
 // ===============================================================================
 // LOAD DATA
 // We are linking our routes to a series of "data" sources.
-const dbJSON = require("../db/db.json"); 
+// const dbJSON = require("../db/db.json"); 
+const fs =require("fs");
+var path = require("path");
 
 
 
@@ -9,20 +11,38 @@ module.exports = function(app) {
     // API GET Requests
     // Below code handles when users "visit" a page.
     app.get("/api/notes", function(req, res) {
-      res.json(dbJSON);
-    });
+      fs.readFile("./db/db.json", "utf8", function (err, data) {
+        if (err) throw err;
+        console.log(data);
+        const newData = JSON.parse(data);
+        res.json(newData);
+    })    
+  });
 
 // User needs to be able to type a note and save it
   app.post("/api/notes", function(req, res) {
     // Note the code here. Our "server" will respond to requests and let users input a note and save it.
     // req.body is available since we're using the body parsing middleware
     // pushes the user response into the sidebar area displaying the title
-    console.log(req);
-    console.log(req.body);
-    dbJSON.push(req.title);
-    dbJSON.push(req.text);
-    res.json;
+    fs.readFile("./db/db.json", "utf8", function (err, data) {
+      if (err) throw err;
+      console.log(data);    
+      const noteList=JSON.parse(data);
+      noteList.push(req.body);
+      console.log(noteList);
 
+      fs.writeFile("./db/db.json", JSON.stringify(noteList), function(err){
+        if(err) throw err;
+        console.log("added");
+        res.redirect("/notes");
+
+      })
+    
+    })
+    // console.log(req.body);
+    // dbJSON.push(req.title);
+    // dbJSON.push(req.text);
+    // res.json;
     
   });
 }; 
--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 // install dependencies 
 const express = require("express");
+// serves our html file
+const path =require("path");
+// const fs =require("fs");
 
 // sets up the express app
 const app = express();
 
 // Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8081;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.get("/",function (req,res){
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+});
+app.get("/notes",function (req,res){
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+});
 
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-
 require("./routes/notes")(app);
 require("./routes/htmlRoutes")(app);
 
